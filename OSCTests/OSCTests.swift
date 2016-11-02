e07c33da4f18bca9720a055a69bdcca8fc19727d
jsbean <jbean46@gmail.com>
2016-11-01 21:40:24 -0400

    Add a few tests ensuring connective tissue in fact connected

--- OSCTests/OSCTests.swift
+++ OSCTests/OSCTests.swift
@@ -14,4 +14,32 @@ class OSCTests: XCTestCase {
     func testMessage() {
         let _ = OSC.Message()
     }
+    
+    func testPacket() {
+        let _ = OSC.Packet()
+    }
+    
+    func testBundle() {
+        let _ = OSC.Bundle()
+    }
+    
+    func testClient() {
+        let _ = OSC.Client()
+    }
+    
+    func testServer() {
+        let _ = OSC.Server()
+    }
+    
+    func testSocket() {
+        let _ = OSC.Socket()
+    }
+    
+    func testParser() {
+        let _ = OSC.Parser()
+    }
+    
+    func testTimeTag() {
+        let _ = OSC.TimeTag()
+    }
 }
