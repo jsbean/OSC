89ca9cae7cab045005b116b99111cd708cd9f70f
jsbean <jbean46@gmail.com>
2016-11-01 21:24:43 -0400

    Add more Swift shadowings

--- OSC/OSC.swift
+++ OSC/OSC.swift
@@ -6,4 +6,13 @@
 //
 //
 
+public typealias Packet = F53OSCPacket
+public typealias PacketDestination = F53OSCPacketDestination
 public typealias Message = F53OSCMessage
+public typealias Bundle = F53OSCBundle
+public typealias Parser = F53OSCParser
+public typealias TimeTag = F53OSCTimeTag
+public typealias Client = F53OSCClient
+public typealias Server = F53OSCServer
+public typealias Socket = F53OSCSocket
+
