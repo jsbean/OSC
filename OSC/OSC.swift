4e02d2bb39ed28931283caf83cb37e4032c172d4
jsbean <jbean46@gmail.com>
2016-11-01 21:16:32 -0400

    Create framework

---
+++ OSC/OSC.swift
@@ -0,0 +1,9 @@
+//
+//  OSC.swift
+//  OSC
+//
+//  Created by James Bean on 11/1/16.
+//
+//
+
+public typealias Message = F53OSCMessage
---
+++ OSCTests/OSCTests.swift
@@ -0,0 +1,17 @@
+//
+//  OSCTests.swift
+//  OSC
+//
+//  Created by James Bean on 11/1/16.
+//
+//
+
+import XCTest
+@testable import OSC
+
+class OSCTests: XCTestCase {
+    
+    func testMessage() {
+        let _ = OSC.Message()
+    }
+}
